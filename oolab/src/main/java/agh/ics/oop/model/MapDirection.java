995d413a5a1cc8f080129e5d7cab2a3b37bcb7b5
Magmusacy <xpfk.098@gmail.com>
2025-01-01 17:35:55 +0100

    Add another 4 map directions and a method that converts unitVector to MapDirection

--- oolab/src/main/java/agh/ics/oop/model/MapDirection.java
+++ oolab/src/main/java/agh/ics/oop/model/MapDirection.java
@@ -1,20 +1,30 @@
 package agh.ics.oop.model;
 
 public enum MapDirection {
-    NORTH,
-    SOUTH,
-    WEST,
-    EAST;
+    NORTH(new Vector2d(0, 1)),
+    NORTHEAST(new Vector2d(1, 1)),
+    NORTHWEST(new Vector2d(-1, 1)),
+    SOUTH(new Vector2d(0, -1)),
+    SOUTHEAST(new Vector2d(1, -1)),
+    SOUTHWEST(new Vector2d(-1, -1)),
+    WEST(new Vector2d(-1, 0)),
+    EAST(new Vector2d(1, 0));
 
-    static final Vector2d unitNorth = new Vector2d(0, 1);
-    static final Vector2d unitSouth = new Vector2d(0, -1);
-    static final Vector2d unitWest = new Vector2d(-1, 0);
-    static final Vector2d unitEast = new Vector2d(1, 0);
+    private final Vector2d unitVector;
 
+    MapDirection(Vector2d unitVector) {
+        this.unitVector = unitVector;
+    }
+
+    @Override
     public String toString() {
         return switch (this) {
             case NORTH -> "Północ";
+            case NORTHEAST -> "Północny Wschód";
+            case NORTHWEST -> "Północny Zachód";
             case SOUTH -> "Południe";
+            case SOUTHEAST -> "Południowy Wschód";
+            case SOUTHWEST -> "Południowy Zachód";
             case EAST -> "Wschód";
             case WEST -> "Zachód";
         };
@@ -22,28 +32,41 @@ public enum MapDirection {
 
     public MapDirection next() {
         return switch (this) {
-            case NORTH -> EAST;
-            case SOUTH -> WEST;
-            case EAST -> SOUTH;
-            case WEST -> NORTH;
+            case NORTH -> NORTHEAST;
+            case NORTHEAST -> EAST;
+            case EAST -> SOUTHEAST;
+            case SOUTHEAST -> SOUTH;
+            case SOUTH -> SOUTHWEST;
+            case SOUTHWEST -> WEST;
+            case WEST -> NORTHWEST;
+            case NORTHWEST -> NORTH;
         };
     }
 
     public MapDirection previous() {
         return switch (this) {
-            case NORTH -> WEST;
-            case SOUTH -> EAST;
-            case EAST -> NORTH;
-            case WEST -> SOUTH;
+            case NORTH -> NORTHWEST;
+            case NORTHWEST -> WEST;
+            case WEST -> SOUTHWEST;
+            case SOUTHWEST -> SOUTH;
+            case SOUTH -> SOUTHEAST;
+            case SOUTHEAST -> EAST;
+            case EAST -> NORTHEAST;
+            case NORTHEAST -> NORTH;
         };
     }
 
     public Vector2d toUnitVector() {
-        return switch (this) {
-            case NORTH -> unitNorth;
-            case SOUTH -> unitSouth;
-            case EAST -> unitEast;
-            case WEST -> unitWest;
-        };
+        return unitVector;
+    }
+
+    public static MapDirection unitVectorToDirection(Vector2d vector) {
+        for (var direction : MapDirection.values()) {
+            if (direction.toUnitVector().equals(vector)) {
+                return direction;
+            }
+        }
+        
+        throw new IllegalArgumentException("No direction for vector: " + vector);
     }
 }
